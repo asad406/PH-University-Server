3ee55ae5c8724ad936fdfe767e984e885dcd00af
asad406 <asadul76@gmail.com>
2024-12-02 11:27:22 +0600

    Higher Order function added insted of try-catch

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -29,66 +29,58 @@ import httpStatus from "http-status";
 //     // console.log(error);
 //   }
 // };
+/*higher order function*/
+const catchAsync = (func: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(func(req, res, next)).catch(err => next(err))
+  }
+}
 // Data receive from student.service
-const getAllStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await StudentServices.getAllStudentFromDB();
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is retrieved successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is retrieved successfully',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-};
-const getSingleStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.getSingleStudentFromDB(studentId);
+const getAllStudent: RequestHandler = catchAsync(async (req, res, next) => {
+  const result = await StudentServices.getAllStudentFromDB();
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is retrieved successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is retrieved successfully',
+    data: result,
+  })
+});
 
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Students are retrieved successfully',
-    //   data: result,
-    // });
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Students are retrieved successfully',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-};
-const deleteSingleStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.deleteSingleStudentFromDB(studentId);
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is deleted successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is deleted successfully',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-};
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(studentId);
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Students are retrieved successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students are retrieved successfully',
+    data: result,
+  })
+});
+const deleteSingleStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.deleteSingleStudentFromDB(studentId);
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is deleted successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is deleted successfully',
+    data: result,
+  })
+});
 export const StudentControllers = {
   //It will use Router
   // createStudent,
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,20 +2,22 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { User } from "./user.model";
 import { UserServices } from "./user.service";
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { password, student: studentData } = req.body
-    const result = await UserServices.createStudentToDB(password, studentData)
-
-    res.status(200).json({
-      message: 'Student created successfully',
-      success: true,
-      result
-    })
-  } catch (err) {
-    next(err)
+/*higher order function*/
+const catchAsync = (func: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(func(req, res, next)).catch(err => next(err))
   }
 }
+
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: studentData } = req.body
+  const result = await UserServices.createStudentToDB(password, studentData)
+  res.status(200).json({
+    message: 'Student created successfully',
+    success: true,
+    result
+  })
+})
 export const UserController = {
   createStudent
 }
