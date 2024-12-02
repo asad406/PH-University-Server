d04534ba7677f4b8c752e9e8f292aa4df6fb9375
asad406 <asadul76@gmail.com>
2024-12-02 07:03:27 +0600

    httpStatus added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { UserRouters } from './modules/user/user.route'
 import { StudentRoutes } from './modules/student/student.route'
 import globalErrorHandler from './middleware/globalErrorHandler'
+import notFound from './middleware/notFound'
 
 const app: Application = express()
 app.use(express.json())
@@ -14,5 +15,7 @@ app.use('/api/students',StudentRoutes)
 
 
 app.use(globalErrorHandler)
+//not found
+app.use(notFound)
 
 export default app
