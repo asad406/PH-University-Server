c949e8db782ed72915ff208eeb8407a0e5e45c6c
asad406 <asadul76@gmail.com>
2024-12-07 06:39:23 +0600

    Semester Code validation added

--- src/modules/academicSemester/academicSemester.service.ts
+++ src/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,19 @@
 import { TAcademicSemester } from "./academicSemester.interface";
 import AcademicSemester from "./academicSemester.model";
 
-const createAcademicSemesterIntoDB = async(payload:TAcademicSemester) => {
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+    type TAcademicSemesterCodeMapper = {
+        [key: string]: string
+    }
+
+    const academicSemesterCodeMapper: TAcademicSemesterCodeMapper = {
+        Autumn: '01',
+        Summer: '02',
+        Fall: '03'
+    }
+    if(academicSemesterCodeMapper[payload.name] !== payload.code){
+        throw new Error ('Invalid Semester code')
+    }
     const result = await AcademicSemester.create(payload);
     return result;
 }
