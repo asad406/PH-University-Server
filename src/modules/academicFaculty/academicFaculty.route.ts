de6c014830bf0fcca80b2265df9dc6d1eb78f87c
asad406 <asadul76@gmail.com>
2024-12-08 07:22:04 +0600

    academicFaculty updated

--- src/modules/academicFaculty/academicFaculty.route.ts
+++ src/modules/academicFaculty/academicFaculty.route.ts
@@ -1,1 +1,15 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+import { AcademicFacultyController } from './academicFaculty.controller';
+const router = express.Router();
 
+router.post('/create-academic-faculty', validateRequest(AcademicFacultyValidation.createAcademicFacultyValidationSchema), AcademicFacultyController.createAcademicFaculty)
+
+router.get('/:facultyId',AcademicFacultyController.getSingleAcademicFaculty)
+
+router.get('/',AcademicFacultyController.getAllAcademicFaculties)
+
+router.patch('/:facultyId', validateRequest(AcademicFacultyValidation.updateAcademicFacultyValidationSchema), AcademicFacultyController.updateAcademicFaculty)
+
+export const AcademicFacultyRoutes = router
