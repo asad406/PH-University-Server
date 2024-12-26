b9ab3a7fb3add83a0c8790956ad0a7ec0319b8ed
asad406 <asadul76@gmail.com>
2024-12-26 18:19:16 +0600

    Coustom zod validation added for time comparing

--- src/modules/offeredCourse/offeredCourse.validation.ts
+++ src/modules/offeredCourse/offeredCourse.validation.ts
@@ -23,6 +23,12 @@ const createOfferedCourseValidationSchema = z.object({
         }, {
             message: 'Invalid time format, expect HH:MM in 24h'
         })
+    }).refine((body) => {
+        const start = new Date(`1970-01-01T${body.startTime}:00`) // startTime: 10:30 => 1970-01-01T10:30:00
+        const end = new Date(`1970-01-01T${body.endTime}:00`)
+        return end > start
+    },{
+        message: "Start time should be before End time!"
     })
 });
 const updateOfferedCourseValidationSchema = z.object({
