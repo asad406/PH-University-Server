cb1db96837f53b9caca9f06e3a7e4c5666e74d22
asad406 <asadul76@gmail.com>
2024-12-30 20:22:19 +0600

    Query builder updated

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -105,7 +105,11 @@ const getAllStudentFromDB = async (query: Record<string, unknown>) => {
     .fields();
 
   const result = await studentQuery.modelQuery;
-  return result;
+  const meta = await studentQuery.countTotal();
+  return {
+    meta,
+    result,
+  };
 };
 
 const getSingleStudentFromDB = async (id: string) => {
